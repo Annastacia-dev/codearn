3c2585b3a7a1da1cdeb2c90a508cf3e876332aeb
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-30 15:15:55 +0300

    add remember me to permitted params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params
-        params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+        params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :remember_me)
     end
 
 
