ee4997c6e33defa42015fd9c809dc0107215b235
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-30 11:17:05 +0300

    authorize

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+
+    validates :first_name, :last_name, :email, :password, presence: true
+    validates :email, uniqueness: true
+   
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/, message: "must be at least 6 characters long, and must contain at least one letter and one number" }
+
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be in a valid format" }
+ 
     has_secure_password
 end
--- config/application.rb
+++ config/application.rb
@@ -31,16 +31,14 @@ module Codearn
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # Cookies And Sessions
-
-    # config.middleware.use ActionDispatch::Cookies
-    # config.middleware.use ActionDispatch::Session::CookieStore
-    # config.action.dispatch.cookies_same_site_protection = :strict
+    config.action_dispatch.cookies_same_site_protection = :strict
 
   end
 end
