8757905320b963d839327a498ac142c166521023
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-30 11:18:04 +0300

    sessions auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(email: params[:email]) || User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: {errors: ["Invalid email or password"]}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+
 end
