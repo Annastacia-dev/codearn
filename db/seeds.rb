a68ac0da3307a3a9a0660d82cd27a9fa8b9fb55f
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-12-01 22:16:38 +0300

    sample template

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,4 +21,19 @@ User.create!(
 
 puts "Done!"
 
+# Templates
 
+puts "Sample Template..."
+
+Template.create!(
+    image_url: "https://assets.startbootstrap.com/img/screenshots/premium/sb-ui-kit-pro-angular.high.webp",
+    live_site:"https://startbootstrap.com/previews/sb-ui-kit-pro-angular",
+    title: "SB UI Kit Pro Angular",
+    description: "SB UI Kit Pro Angular is a fully developed front-end UI toolkit created with Angular 13. Built for developers, by developers, this is much more than just a front-end theme. It includes a full set of components, plugins, and more to help you build your next project.",
+    features: "Intricate Routing, Custom Components, Unit Tests & Functional Tests, Schematics",
+    github_link:"https://github.com/evgensharyy/nglp-angular-material-landing-page",
+    category: "Landing Page",
+    technologies: "Angular"
+)
+
+puts "Done!"
