351ee98b04ba9beb732cd37f0dacd0fecf0b6f40
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-12-08 16:53:36 +0300

    render deployment

--- Procfile
+++ Procfile
@@ -1,8 +1,2 @@
-web: PORT=4000 npm start --prefix client
-api: PORT=3000 rails s
-
-
-
-
-
-# web: rake db:migrate && bin/rails server -b 0.0.0.0 -p $PORT (For Railway deployment)
+web: bundle exec rails s
+release: bin/rake db:migrate
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,21 +67,6 @@ config.active_storage.service = :local
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
