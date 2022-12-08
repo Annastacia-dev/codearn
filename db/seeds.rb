cdc5b68956c4bdf0b060e090c726a680590306e2
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-12-08 17:54:46 +0300

    remove seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,39 +8,4 @@
 
 #User
 
-puts "Sample Users..."
-
-User.create!(
-    first_name: "Admin",
-    last_name: "Admin",
-    username: "admin",
-    email: "admin@gmail.com",
-    password: "@Dmin321",
-    password_confirmation: "@Dmin321",
-    seller: true,
-)
-
-User.create!(
-    first_name: "John",
-    last_name: "Doe",
-    username: "johndoe",
-    email: "johndoe@gmail.com",
-    password: "@Johnny32",
-    password_confirmation: "@Johnny32",
-    premium: false,
-    seller: true
-)
-
-User.create!(
-    first_name: "Jane",
-    last_name: "Smith",
-    username: "janesmith",
-    email: "jane@gmail.com",
-    password: "@Jane123",
-    password_confirmation: "@Jane123",
-    premium: true,
-    seller: false
-)
-
-puts "Done!"
 
