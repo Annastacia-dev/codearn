420846c13b5a2d6c2df20bcecebc7bddf712cf7c
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-12-08 18:18:08 +0300

    fix errors

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,5 +1,9 @@
-class FallbackController < ApplicationController::Base
+# Controller logic: fallback requests for React Router.
+# Leave this here to help deploy your app later!
+class FallbackController < ActionController::Base
+
   def index
+    # React app index page
     render file: 'public/index.html'
   end
 end
--- client/src/components/PremiumPay.js
+++ client/src/components/PremiumPay.js
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react'
+import React,{ useState} from 'react'
 import Brand from './Brand'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
