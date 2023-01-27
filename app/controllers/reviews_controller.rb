76d76a2369c6c9d95656ce5987d6aec06767ed53
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-27 18:40:38 +0300

    add actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,20 @@ class ReviewsController < ApplicationController
         @review = Review.create!(review_params)
         render json: @review, status: :created
     end
+
+    def show 
+        @review = Review.find(params[:id])
+        render json: @review, status: :ok
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        if @review.update(review_params)
+            render json: @review, status: :ok
+        else
+            render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
     
     def destroy
         @review = Review.find(params[:id])
